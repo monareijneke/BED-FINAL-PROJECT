ceb42b83a679d5e820d301cbe6064cd2f1e6c281
Mona Reijneke <cooljay.mr@gmail.com>
2024-01-30 22:17:03 +0100

    added Host queries

---
+++ src/services/hosts/createHost.js
@@ -0,0 +1,25 @@
+import { PrismaClient } from "@prisma/client";
+
+const createHost = async (
+  username,
+  password,
+  name,
+  email,
+  phoneNumber,
+  profilePicture,
+  aboutMe
+) => {
+  const prisma = new PrismaClient();
+  const host = await prisma.host.create({
+    data: {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    },
+  });
+};
+export default createHost;
