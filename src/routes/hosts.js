8c93892ab3da29a5b0d73218d7fc92d14fbc095b
Mona Reijneke <cooljay.mr@gmail.com>
2024-01-30 22:15:32 +0100

    added Host queries

--- src/routes/hosts.js
+++ src/routes/hosts.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
 import getHosts from "../services/hosts/getHosts.js";
 import getHostById from "../services/hosts/getHostById.js";
+import createHost from "../services/hosts/createHost.js";
+import updateHost from "../services/hosts/updateHost.js";
+import deleteHost from "../services/hosts/deleteHost.js";
+import auth from "../utils/auth.js";
 
 const router = Router();
 
@@ -22,6 +26,82 @@ router.get("/:id", async (req, res, next) => {
       res.status(404).json({ message: `Couldn't find host with id ${id}!` });
     } else {
       res.status(200).json(host);
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", auth, async (req, res, next) => {
+  try {
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const newHost = await createHost(
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe
+    );
+    res.status(201).json(newHost);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", auth, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    } = req.body;
+    const host = await updateHost(id, {
+      username,
+      password,
+      name,
+      email,
+      phoneNumber,
+      profilePicture,
+      aboutMe,
+    });
+    if (!host) {
+      res.status(404).json({ message: `Could not found host with id ${id}` });
+    } else {
+      res
+        .status(200)
+        .json({ message: `Host with id ${id} is updated correctly` });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", auth, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const host = await deleteHost(id);
+
+    if (!host) {
+      res.status(404).json({ messgae: `Host with id ${id} not found` });
+    } else {
+      res
+        .status(200)
+        .json({ message: `Host with id ${id} succesfully deleted` });
     }
   } catch (error) {
     next(error);
