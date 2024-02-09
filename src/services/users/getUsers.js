7054cf0e578abe84f4e96a8b32f14d6559c7df2d
Mona Reijneke <cooljay.mr@gmail.com>
2024-02-09 18:38:46 +0100

    adding endpoints with arguments

--- src/services/users/getUsers.js
+++ src/services/users/getUsers.js
@@ -1,10 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const getUsers = async () => {
+const getUsers = async (username, email) => {
   const prisma = new PrismaClient();
   const users = await prisma.user.findMany({
-    where: {},
+    where: {
+      username,
+      email,
+    },
   });
+
   return users;
 };
 
