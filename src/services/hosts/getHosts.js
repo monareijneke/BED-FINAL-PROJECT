27242f3aaf48de30d3fa06f2f631c110fee4b89f
Mona Reijneke <cooljay.mr@gmail.com>
2024-02-09 18:36:32 +0100

    adding endpoints with arguments

--- src/services/hosts/getHosts.js
+++ src/services/hosts/getHosts.js
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const getHosts = async () => {
+const getHosts = async name => {
   const prisma = new PrismaClient();
   const hosts = await prisma.host.findMany({
-    where: {},
+    where: { name },
   });
   return hosts;
 };
