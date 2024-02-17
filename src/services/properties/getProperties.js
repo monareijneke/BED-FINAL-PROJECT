439faf5390202db0f453ce14668550e571910fae
Mona Reijneke <cooljay.mr@gmail.com>
2024-02-17 11:08:54 +0100

    Final Commit cleansing

--- src/services/properties/getProperties.js
+++ src/services/properties/getProperties.js
@@ -1,9 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-const getProperties = async () => {
+const getProperties = async (location, pricePerNight, amenitis) => {
   const prisma = new PrismaClient();
   const property = await prisma.property.findMany({
-    where: {},
+    where: { location, pricePerNight, amenitis },
   });
   return property;
 };
